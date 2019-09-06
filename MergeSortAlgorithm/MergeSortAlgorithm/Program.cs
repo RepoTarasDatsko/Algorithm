d94b5434f78bb943d73e85ed33cbb9cf3c202f8e
Тарас <@DESKTOP-PGEAAM8>
2019-09-06 09:08:27 +0300

    Save Quick and Merge algorithm

---
+++ MergeSortAlgorithm/MergeSortAlgorithm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergeSortAlgorithm
+{
+    class Program
+    {
+        public static void MergeSort(List<int>seq, int start, int stop)
+        {
+            if (start >= stop - 1)
+            {
+                return;
+            }
+
+            int mid = (start + stop) / 2;
+     
+            MergeSort(seq, start, mid);
+            MergeSort(seq, mid, stop);
+            merge(seq, start, mid, stop);
+        }
+        public static void merge(List<int> seq, int start, int mid, int stop )
+        {
+            List<int> temp = new List<int>();
+            int i = start;
+            int j = mid;
+            while(i < mid && j < start)
+            {
+                if (seq[i] < seq[j])
+                {
+                    temp.Add(seq[i]);
+                    i++;
+                }
+                else
+                {
+                    temp.Add(seq[j]);
+                    j++;                }
+
+            }
+
+            while(i < mid)
+            {
+                temp.Add(seq[i]);
+                i++;
+            }
+
+            for(i = 0; i > temp.Count; i++)
+            {
+                seq[start + i] = temp[i];
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            List<int> seq = new  List<int>{ 9, 8, 7, 5, 6, 4, 2, 3, 1 };
+
+            MergeSort(seq, 0, seq.Count);
+
+            Console.WriteLine(string.Join(",", seq));
+        }
+    }
+}
---
+++ QuickSortAlgorithm/QuickSortAlgorithm/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickSortAlgorithm
+{
+    class Program
+    {
+        public static int partition(List<int> seq, int start, int stop)
+        {
+            int indexPivot = start;
+            int pivot = seq[indexPivot];
+            int i = start + 1;
+            int j = stop - 1;
+
+            while (i <= j)
+            {
+                while (i <= j && !(pivot < seq[i]))
+                {
+                    i++;
+                }
+                while (i <= j && pivot < seq[j])
+                {
+                    j--;
+                }
+                if (i < j)
+                {
+                    int temp = seq[i];
+                    seq[i] = seq[j];
+                    seq[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            seq[indexPivot] = seq[j];
+            seq[j] = pivot;
+
+            return j;
+        }
+
+        public static void QuickSort(List<int> seq, int start, int stop)
+        {
+            if (start >= stop - 1)
+            {
+                return;
+            }
+            int indexPivot = partition(seq, start, stop);
+            QuickSort(seq, start, indexPivot);
+            QuickSort(seq, indexPivot + 1, stop);
+            
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> seq = new List<int>() { 2, 3, 6, 5, 1, 8, 9, 4, 7 };
+            QuickSort(seq, 0, seq.Count);
+            Console.WriteLine(string.Join(",", seq));
+        }
+    }
+}
