fd8fb46d13b8969d7b083bf6bb2ea1ad1567a97b
Тарас <@DESKTOP-PGEAAM8>
2019-09-09 09:53:35 +0300

    Add algorithm

---
+++ DepthFirstSearchAlgorithm/DepthFirstSearchAlgorithm/Algorithms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepthFirstSearchAlgorithm
+{
+    public class Algorithms
+    {
+        public HashSet<char> DFS(Graph graph, char start)
+        {
+            var visited = new HashSet<char>();
+
+            var stack = new Stack<char>();
+            stack.Push(start);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                    if (!visited.Contains(neighbor))
+                        stack.Push(neighbor);
+            }
+            return visited;
+          
+        }
+    }
+}
---
+++ DepthFirstSearchAlgorithm/DepthFirstSearchAlgorithm/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DepthFirstSearchAlgorithm
+{
+    public class Graph
+    {
+        public Graph(char[] vertices, Tuple<char, char>[] edges)
+        {
+            foreach (var vertex in vertices)
+            {
+                AddVertix(vertex);
+            }
+
+            foreach (var edge in edges)
+                AddEdge(edge);
+        }
+        public Dictionary<char , HashSet<char>> AdjacencyList { get; } = new Dictionary<char, HashSet<char>>();
+
+        public void AddVertix(char vertix)
+        {
+            AdjacencyList[vertix] = new HashSet<char>();
+        }
+        public void AddEdge(Tuple<char, char> edge)
+        {
+            if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
+            {
+                AdjacencyList[edge.Item1].Add(edge.Item2);
+                AdjacencyList[edge.Item2].Add(edge.Item1);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var vertices = new[] { 'A', 'B', 'C', 'D', 'E' };
+            var edges = new[] {Tuple.Create('A', 'B'), Tuple.Create('A','C'),
+                Tuple.Create('B','D'), Tuple.Create('C', 'E'),
+                 Tuple.Create('D','A'), Tuple.Create('E', 'D')};
+            var algorithms = new Algorithms();
+            var graph = new Graph(vertices, edges);
+            Console.WriteLine(string.Join(",", algorithms.DFS(graph, 'A')));
+            //    List<String> names = new List<String>();
+            //    names.Add("Bruce");
+            //    names.Add("Alfred");
+            //    names.Add("Tim");
+            //    names.Add("Richard");
+
+            //    // Display the contents of the list using the Print method.
+            //    //names.ForEach(Print);
+
+            //    // The following demonstrates the anonymous method feature of C#
+            //    // to display the contents of the list to the console.
+            //    names.ForEach((name) =>  { Console.WriteLine(name); });
+            //}
+            //private static void Print(string s)
+            //{
+            //    Console.WriteLine(s);
+            //}
+        }
+    }
+}
